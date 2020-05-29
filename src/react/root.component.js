2f57004fd2a51b81c1b7c59a82515832da01bd3f
dbw <dbw@dbw.dev>
2020-05-28 22:15:57 -0700

    Initial commit

---
+++ src/react/root.component.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="react-container">
+      <h1>Microfrontends POC using React and Vue</h1>
+      <article className="react-article">
+        <p>This is some text writen in a React app</p>
+        <p>
+          Sit dicta nihil ut tempora accusantium. Delectus ad optio maiores
+          soluta officiis pariatur. Impedit laborum enim sit sit ipsum Molestiae
+          autem expedita accusantium quaerat animi. Nostrum sint neque alias
+          laboriosam accusamus, enim Sequi expedita laudantium ab dolorum
+          voluptas magnam Beatae nobis natus explicabo accusantium natus non?
+          Quidem sunt aut omnis!
+        </p>
+      </article>
+      <button className="react-btn">I'm a React button</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/vue/main.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="vue-nav">
+    <h2>I'm a navbar in Vue</h2>
+    <ul class="nav-menu">
+      <li><a href="/">One</a></li>
+      <li><a href="/">Two</a></li>
+      <li><a href="/">Three</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+.vue-nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background: mistyrose;
+  padding: 0.25rem;
+}
+
+.nav-menu {
+  list-style-type: none;
+}
+
+.nav-menu li {
+  display: inline;
+  margin: 0 0.2rem;
+}
+</style>
